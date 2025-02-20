925db924ce588181b39ba90b5bad285fdb01c328
0 <0>
2025-02-20 10:50:42 +0100

    Yes 2

--- skeleton_project/source/Elevator.c
+++ skeleton_project/source/Elevator.c
@@ -1,7 +1,14 @@
 #include "Elevator.h"
 
+void openDoor(Elevator *p_elevator)
+{
+    elevio_doorOpenLamp(1);
+    p_elevator->door_open = 1;
+}
+
 void closeDoor(Elevator *p_elevator)
 {   
+    elevio_doorOpenLamp(0);
     p_elevator->door_open = 0;
 }
 
@@ -13,4 +20,10 @@ void updateCurrentFloor(Elevator *p_elevator)
 void setMotorDir(Elevator *p_elevator, MotorDirection dir)
 {
     p_elevator->motor_dir = dir;
+}
+
+void emergencyStop(Elevator *p_elevator)
+{
+    elevio_motorDirection(DIRN_STOP);
+    p_elevator->motor_dir = DIRN_STOP;
 }
