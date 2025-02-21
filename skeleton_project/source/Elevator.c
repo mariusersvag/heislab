a2b7b235c1a1e3b695b17a8eeb21fb342b20f2a3
Eskil Fromholtz <eskilaf@stud.ntnu.no>
2025-02-21 08:55:50 +0100

    Some changes

--- skeleton_project/source/Elevator.c
+++ skeleton_project/source/Elevator.c
@@ -7,9 +7,19 @@ void stop(Elevator *p_elevator)
     elevio_stopLamp(1);
 }
 
+
+
 Elevator createElevator()
 {   
-    Elevator elev = {};
+    int c_f = elevio_floorSensor();    
+    int p_f;
+
+    if (c_f == 1) p_f = 2;
+    else if (c_f == 2) p_f = 1;
+    else if (c_f == 3) p_f = 2;
+    else if (c_f == 4) p_f = 3;
+
+    Elevator elev = {c_f, p_f, 0, DIRN_STOP};
     return elev;
 }
 
@@ -20,25 +30,34 @@ void moveTo(Elevator *p_elevator, int floor)
 {
     updateCurrentFloor(p_elevator);
 
-    if ((p_elevator->current_floor == 4) || (p_elevator->current_floor > floor)){
-        setMotorDir(p_elevator, DIRN_DOWN);
+    //Arrived
+    if (p_elevator->current_floor == floor) {
+        setMotorDir(p_elevator, DIRN_STOP);
+        // arrived(p_elevator);
     }
-    else {
+
+    //Queue is empty
+    if (floor == -1) {
+        setMotorDir(p_elevator, DIRN_STOP);
+    }
+
+    //Normal mode
+    if ((p_elevator->current_floor > floor)){
+        setMotorDir(p_elevator, DIRN_DOWN);
+    } else {
         setMotorDir(p_elevator, DIRN_UP);
     }
-
-    if (p_elevator->current_floor == floor) {
-        arrived(p_elevator);
-    }
-
 }
 
 
 
 void openDoor(Elevator *p_elevator)
 {
-    elevio_doorOpenLamp(1);
-    p_elevator->door_open = 1;
+
+    if (p_elevator->motor_dir == DIRN_STOP) {
+        elevio_doorOpenLamp(1);
+        p_elevator->door_open = 1;
+    }
 }
 
 
@@ -46,10 +65,20 @@ void openDoor(Elevator *p_elevator)
 void closeDoor(Elevator *p_elevator)
 {   
     bool obs = elevio_obstruction();
-    if (obs) handleObstruction(p_elevator);
+    if (obs) {
+        handleObstruction(p_elevator);
+    } else {
+        elevio_doorOpenLamp(0);
+        p_elevator->door_open = 0;
+    }
+}
 
-    elevio_doorOpenLamp(0);
-    p_elevator->door_open = 0;
+
+void handleObstruction(Elevator *p_elevator)
+{
+    //Sleep for 3s
+    sleep(3);
+    closeDoor(p_elevator);
 }
 
 
@@ -77,11 +106,5 @@ void emergencyStop(Elevator *p_elevator)
 
 
 
-void handleObstruction(Elevator *p_elevator)
-{
-    //Sleep for 3s
-    sleep(3);
-    closeDoor(p_elevator);
-}
 
 
